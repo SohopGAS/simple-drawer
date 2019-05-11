648a8db130a82be197ff2e6185d983113ab5c20e
Gal Argov Sofer <29959237+SohopGAS@users.noreply.github.com>
2019-05-11 11:24:02 +0300

    Delete old comment lines.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -142,34 +142,6 @@ namespace ex1Grafics
                 }
                     }catch(Exception) { return; }
         }
-        //private void ZeichneBezier(int n, System.Drawing.Point P1, System.Drawing.Point P2, System.Drawing.Point P3, bool initial)
-        //{
-        //    Graphics g = canvas.Graphics;
-        //    System.Drawing.Pen bkStift = new System.Drawing.Pen(System.Drawing.Color.Red, 2);
-        //    System.Drawing.Pen kpStift = new System.Drawing.Pen(System.Drawing.Color.Black, 3);
-
-        //    if (initial)
-        //    {
-        //        g.DrawLine(kpStift, P1, P2);
-        //        g.DrawLine(kpStift, P2, P3);
-        //    }
-
-        //    if (n > 0)
-        //    {
-        //        System.Drawing.Point P12 = new System.Drawing.Point((P1.X + P2.X) / 2, (P1.Y + P2.Y) / 2);
-        //        System.Drawing.Point P23 = new System.Drawing.Point((P2.X + P3.X) / 2, (P2.Y + P3.Y) / 2);
-        //        System.Drawing.Point P123 = new System.Drawing.Point((P12.X + P23.X) / 2, (P12.Y + P23.Y) / 2);
-
-        //        ZeichneBezier(n - 1, P1, P12, P123, pva, false);
-        //        ZeichneBezier(n - 1, P123, P23, P3, pva, false);
-        //    }
-        //    else
-        //    {
-        //        g.DrawLine(bkStift, P1, P2);
-        //        g.DrawLine(bkStift, P2, P3);
-        //    }
-        //}
-
         public Line nline(System.Windows.Point p1, System.Windows.Point p2)
         {
 
